b51a1dd446dfd376c037cd00450859124a00d969
Anton Shein <adrior@ya.ru>
2014-01-07 18:36:01 +0400

    Keep working

--- demo/blocks/sample/sample.js
+++ demo/blocks/sample/sample.js
@@ -1,10 +1,49 @@
-eb.decl('bSample', {
-	template: {
-		title: 'title',
-		desctiption: 'text'
-		actions: {
-			ok: {bButton: 'OK'},
-			cancel: {bButton: 'Cancel'}
-		}
-	},
-});
+var bSample = {
+	title: 'title',
+	desctiption: 'text'
+	actions: {
+		ok: {bButton: 'OK'},
+		cancel: {bButton: 'Cancel'}
+	}
+};
+
+bSample.template = {
+
+}
+
+bSample.tamplates['__title'] = {
+	bLink: {
+		url: '%../url%',
+		text: '%.%'	
+	}
+}
+
+function bSample()
+
+
+bSnippet = {
+	title: 'Заголовок сниппета',
+	url: 'siteurl.com',
+	text: 'Текст сниппета',
+	sitelink: [
+		{name: 'Посещаемая', url: 'popular.html'},
+		{name: 'Авторизация', url: 'auth.html'},
+	]
+}
+
+bSnippet = {
+	title: '%./title%',
+	url: '%url%',
+	text: '%text%' + ' Читатеть полностью',
+	sitelinks: {
+		tag: 'p',
+		content: '%sitelinks%'
+	}
+}
+
+bSnippet__sitelinks = {
+	bSitelink: {
+		title: '%name%',
+		link: '%../url/%+%url%'
+	}
+}
